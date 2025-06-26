36a0df4ef4cb5091dbb88ea9bc4be62eb37a5ea3
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-06-26 10:59:49 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -21,4 +21,13 @@ app.get("/courses", async (request: Request, response: Response) => {
   response.json(courses)
 })
 
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const {id} = request.params
+  const {name} = request.body
+
+  await knex("courses").update({name}).where({id})
+
+  return response.json()
+})
+
 app.listen(3333, () => console.log(`Server is running on port 3333`))
