fb8133977130b68f4d0a698e822ea42c6604cabe
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-06-26 10:35:55 -0300

    Select

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import {knex} from "./database/knex"
+import { request } from "http"
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,13 @@ app.post("/courses", async (request: Request, response: Response) => {
   //await knex.raw("INSERT INTO courses (name) VALUES (?)", [name])
 
   response.status(201).json()
+})
+
+app.get("/courses", async (request: Request, response: Response) => {
+  //const courses = await knex.raw("SELECT * FROM courses")
+  const courses = await knex("courses").select().orderBy("name")
+
+  response.json(courses)
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
