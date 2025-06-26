ad2eecd5d6d86d3d9ef95c80060d5db50b11cfb7
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-06-26 16:28:04 -0300

    registering modules

--- src/database/migrations/20250626184026_create-course-modules.ts
+++ src/database/migrations/20250626184026_create-course-modules.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
         table.increments("id").primary(),
         table.text("name").notNullable(),
 
-        table.integer("course_id").references("id").inTable("courses")
+        table.integer("course_id").notNullable().references("id").inTable("courses")
     })
 }
 
